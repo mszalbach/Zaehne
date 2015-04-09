bf05807459eb48d2bfb7b37b4cdd709ac3edba33
mszalbach <marcel.szalbach@gmail.com>
2015-04-09 18:46:51 +0200

    - added simple Junittest

---
+++ src/test/java/randomImage/RandomImageTest.java
@@ -0,0 +1,17 @@
+package randomImage;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by Marcel on 09.04.2015.
+ */
+public class RandomImageTest {
+
+    @Test
+    public void testIsValid() {
+        assertThat(1, is(1));
+    }
+}
