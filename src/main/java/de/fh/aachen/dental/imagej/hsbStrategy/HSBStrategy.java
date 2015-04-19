35862f4186e9da361c427e87f18597ac7437523d
mszalbach <marcel.szalbach@gmail.com>
2015-04-19 10:37:44 +0200

    -added hint how to use the interface

--- src/main/java/de/fh/aachen/dental/imagej/hsbStrategy/HSBStrategy.java
+++ src/main/java/de/fh/aachen/dental/imagej/hsbStrategy/HSBStrategy.java
@@ -6,5 +6,9 @@ import ij.ImagePlus;
  * Created by Marcel on 19.04.2015.
  */
 public interface HSBStrategy {
+    /**
+     * @param image to convert to HSB space
+     * @return Object with Hue, Saturation and Brightness Array. Only byte[],short[], float[], int[] are supported by ImageJ API
+     */
     Object[] execute(ImagePlus image);
 }
