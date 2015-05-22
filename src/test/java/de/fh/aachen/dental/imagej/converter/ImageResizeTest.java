6f7e5970cb933c770fe77ff5544facef7c32f0ef
mszalbach <marcel.szalbach@gmail.com>
2015-05-22 22:41:32 +0200

    - added simple UnitTests

---
+++ src/test/java/de/fh/aachen/dental/imagej/converter/ImageResizeTest.java
@@ -0,0 +1,41 @@
+package de.fh.aachen.dental.imagej.converter;
+
+import ij.ImagePlus;
+import ij.gui.NewImage;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by foobar on 22.05.15.
+ */
+public class ImageResizeTest {
+    ImagePlus image;
+
+    @Before
+    public void init() {
+        image = NewImage.createByteImage("Resizable Image", 1000, 1000, 1, NewImage.FILL_BLACK);
+    }
+
+
+    @Test
+    public void test_resize() throws InterruptedException {
+        ImagePlus resizedImage = new ImageResize(500,500,2).convert(image);
+        assertThat(resizedImage.getWidth(),is(500) );
+        assertThat(resizedImage.getHeight(),is(500) );
+    }
+
+    @Test
+    public void test_no_resize() throws InterruptedException {
+        ImagePlus resizedImage = new ImageResize(1500,1500,2).convert(image);
+        assertThat(resizedImage.getWidth(),is(1000) );
+        assertThat(resizedImage.getHeight(),is(1000) );
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_resize_with_factor_one() throws InterruptedException {
+        ImagePlus resizedImage = new ImageResize(1500,1500,1).convert(image);
+    }
+}
---
+++ src/test/java/de/fh/aachen/dental/imagej/converter/ImageTo8BitTest.java
@@ -0,0 +1,23 @@
+package de.fh.aachen.dental.imagej.converter;
+
+import ij.ImagePlus;
+import ij.gui.NewImage;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by foobar on 22.05.15.
+ */
+public class ImageTo8BitTest {
+
+    @Test
+    public void to8Bit() throws InterruptedException {
+        ImagePlus image = NewImage.createRGBImage("Color Image", 1000, 1000, 1, NewImage.FILL_RANDOM);
+        assertThat(image.getProcessor().isGrayscale(), is(false));
+        ImagePlus grayImage = new ImageTo8Bit().convert(image);
+
+        assertThat(grayImage.getProcessor().isGrayscale(),is(true) );
+    }
+}
