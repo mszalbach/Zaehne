38010e40d561b489f130595a7a594fab56e340e7
mszalbach <marcel.szalbach@gmail.com>
2015-06-03 17:37:51 +0200

    - added ImageEdge classes and component

--- src/main/java/de/fh/aachen/dental/Zaehne.java
+++ src/main/java/de/fh/aachen/dental/Zaehne.java
@@ -25,9 +25,9 @@ public class Zaehne extends PlugInFrame {
 
 		converterPanels.add(new ImageDuplicatorComponent());
 		converterPanels.add(new ImageResizeComponent());
+        converterPanels.add(new ReflectionFilterComponent());
 		converterPanels.add(new ImageTo8BitComponent());
-		converterPanels.add(new ReflectionFilterComponent());
-		converterPanels.add(new FJEdgeDetectionComponent());
+        converterPanels.add(new ImageEdgeComponent());
 		converterPanels.add(new ConnectedRegionsComponent());
 		converterPanels.add(new ConnectRegionsComponent());
 
